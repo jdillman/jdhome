7cdaea6e435917da25737ebd43f019506dd4e97f
Jon Dillman <jdd619@gmail.com>
2017-10-08 14:49:57 -0700

    remove superflous code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from 'configureStore';
 import registerServiceWorker from 'registerServiceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from 'components/App';
-
-const store = configureStore();
 
 /* eslint-disable react/jsx-filename-extension */
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <App />,
   document.getElementById('root'),
 );
 registerServiceWorker();
